938d28e5bab21f90d9a8a60c331a5f9204fde2e4
nmtu91174 <nmtu91174@gmail.com>
2025-10-23 19:31:14 +0700

    test checkout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,14 +2,16 @@ import HeroSection from "../components/HeroSection";
 import SectionFour from "../components/SectionFour";
 import SectionThree from "../components/SectionThree";
 import SectionTwo from "../components/SectionTwo";
+import Cart from "./Cart";
 
 export default function Homepage() {
   return (
     <>
       <HeroSection />
       <SectionTwo />
-      <SectionThree/>
-      <SectionFour/>
+      <SectionThree />
+      <SectionFour />
+      <Cart />
     </>
   );
 }
