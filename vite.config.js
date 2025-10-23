4c01c92ce600bfd27e0840c853dbdc1c8b5d1e85
nmtu91174 <nmtu91174@gmail.com>
2025-10-23 17:19:24 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/Reactjs_03_Ruou/',
+  base: '/Reactjs_03_Ruou/',
   plugins: [react()],
 })
